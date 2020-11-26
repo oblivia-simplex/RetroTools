d5b9c3b54d89579703dc3766b1df217b9d082811
lucca <lucca.fraser@gmail.com>
2020-11-26 12:14:20 -0400

    database

--- src/RetroTools.jl
+++ src/RetroTools.jl
@@ -1,9 +1,20 @@
 module RetroTools
+__precompile__(false)
 
 using StatsBase
 using PyCall
 Retro = pyimport("retro")
 
+export poke, peek, mem_read, mem_write, mem_blocks, random_address
+
+function kill(env)
+    env.render(close=true)
+    env.close()
+end
+
+function poke(env, address, value)
+    poke(env, address=address, value=value)
+end
 
 function poke(env; address::Integer, value::Union{UInt8, UInt16})
     if value isa UInt8
@@ -27,14 +38,15 @@ function mem_read(env; address, size=1)
     if isempty(blocks)
         error("Unmapped address. Mapped addresses given by mem_blocks().")
     end
+    address += 1 # julia is 1-indexed
     block = blocks[1]
-    offset = (address - block.start) + 1 # julia is 1-indexed
+    offset = address - block.start
     Vector{UInt8}(env.data.memory.blocks[block.start][offset:(offset+size-1)])
 end
 
 
 function mem_blocks(env)
-    [(p.first):(p.first + length(p.second)) for p in env.data.memory.blocks]
+    [UInt32(p.first):UInt32(p.first + length(p.second) - 1) for p in env.data.memory.blocks]
 end
 
 function random_address(env)
